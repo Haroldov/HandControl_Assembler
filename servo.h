621bd98a1392d6f6f0b9c7ff6c66f1385f57d963
kacunar <32245492+kacunar@users.noreply.github.com>
2017-09-24 14:51:41 -0500

    Add files via upload
    Servo's Header.

---
+++ servo.h
@@ -0,0 +1,69 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+void servoRotate0() //0 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    RB2 = 1;
+    __delay_us(800);
+    RB2 = 0;
+    __delay_us(19200);
+  }
+}
+
+void servoRotate90() //90 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    RB2 = 1;
+    __delay_us(1500);
+    RB2 = 0;
+    __delay_us(18500);
+  }
+}
+
+void servoRotate180() //180 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    RB2 = 1;
+    __delay_us(2000);
+    RB2 = 0;
+    __delay_us(18000);
+  }
+}
